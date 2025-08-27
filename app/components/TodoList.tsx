c718249ead5e434647921b38756d880097b82fb0
Ziyue Lin <ziyue.lin.dev@gmail.com>
2025-08-27 16:35:44 +1000

    update

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,25 +1,30 @@
-const TodoList = () => {
-  return (
-    <div className="overflow-x-auto">
-  <table className="table w-full">
-    {/* head*/}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <td>Cy Ganderton</td>
-        <td>Blue</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-  )
+import { ITask } from "@/types/tasks";
+import React from "react";
+import Task from "./Task";
+
+interface TodoListProps {
+  tasks: ITask[];
 }
 
-export default TodoList
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  return (
+    <div className='overflow-x-auto'>
+      <table className='table w-full'>
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Tasks</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TodoList;
